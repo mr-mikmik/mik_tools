a78b59cd45ab2a05a28317ee3e5e7c331d0cffc0
mr-mikmik <mrmikmik95@gmail.com>
2024-07-10 21:36:05 -0400

    adding shuffle option to LightingDatasetWrapper

--- mik_tools/mik_learning_tools/datasets/lightning_dataset_wrapper.py
+++ mik_tools/mik_learning_tools/datasets/lightning_dataset_wrapper.py
@@ -8,7 +8,7 @@ from argparse import ArgumentParser
 class LightningDatasetWrapper(pl.LightningDataModule):
 
     def __init__(self, dataset, batch_size=None, val_batch_size=None, seed=0, train_fraction=0.8, val_fraction=None, train_size=None,
-                 val_size=None, test_size=0, drop_last=True, num_workers=8, pin_memory=False):
+                 val_size=None, test_size=0, drop_last=True, num_workers=8, pin_memory=False, shuffle=False):
         self.dataset = dataset
         self.batch_size = batch_size
         self.val_batch_size = val_batch_size
@@ -23,6 +23,7 @@ class LightningDatasetWrapper(pl.LightningDataModule):
         self.drop_last = drop_last
         self.num_workers = num_workers
         self.pin_memory = pin_memory
+        self.shuffle = shuffle
         self.train_data = None # Filled on setup
         self.val_data = None   # Filled on setup
         self.test_data = None  # Filled on setup
@@ -61,14 +62,13 @@ class LightningDatasetWrapper(pl.LightningDataModule):
         if batch_size is None:
             batch_size = len(data)
         batch_size = min(batch_size, len(data))
-        loader = DataLoader(data, batch_size=batch_size, num_workers=self.num_workers, drop_last=self.drop_last, pin_memory=self.pin_memory)
+        loader = DataLoader(data, batch_size=batch_size, num_workers=self.num_workers, drop_last=self.drop_last, pin_memory=self.pin_memory, shuffle=self.shuffle)
         return loader
 
     @staticmethod
     def add_specific_args(parent_parser):
         parser = get_datamodule_specific_args(parent_parser)
         return parser
-
 
 
 def get_datamodule_specific_args(parent_parser):
@@ -83,4 +83,5 @@ def get_datamodule_specific_args(parent_parser):
     parser.add_argument("--test_size", type=int, default=0, help='Number of data samples in dataset used for testing. Default: 0')
     parser.add_argument("--drop_last", type=bool, default=True, help='Option to exclude the last batch that does not have full batch size. Default: True')
     parser.add_argument("--pin_memory", action='store_true', help='If true, the dataloaders automatically puts the fetched data Tensors in pinnend memory -- faster data transfers to CUDA-enabled GPus.. Default: False')
+    parser.add_argument("--shuffle", action='store_true', help='If true, the dataloaders automatically puts the fetched data Tensors in pinnend memory -- faster data transfers to CUDA-enabled GPus.. Default: False')
     return parser
