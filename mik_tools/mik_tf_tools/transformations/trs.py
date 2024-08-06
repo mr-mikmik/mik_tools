c118c8e95692c5134ed9eec10a61800a766b72fc
mr-mikmik <mrmikmik95@gmail.com>
2024-08-06 14:11:18 -0400

    batching quaternion multiply

--- mik_tools/mik_tf_tools/transformations/trs.py
+++ mik_tools/mik_tf_tools/transformations/trs.py
@@ -481,17 +481,22 @@ def quaternion_from_matrix_batched_tensor(matrix_in: torch.Tensor) -> torch.Tens
     return quaternions_xyzw
 
 
-def quaternion_multiply(quaternion1, quaternion0):
+def quaternion_multiply(quaternion1:Union[torch.Tensor, np.ndarray], quaternion0:Union[torch.Tensor, np.ndarray]) -> Union[torch.Tensor, np.ndarray]:
     """
     Return multiplication of two quaternions.
     """
-    x0, y0, z0, w0 = quaternion0
-    x1, y1, z1, w1 = quaternion1
-    return np.array((
+    x0, y0, z0, w0 = quaternion0[..., 0], quaternion0[..., 1], quaternion0[..., 2], quaternion0[..., 3]
+    x1, y1, z1, w1 = quaternion1[..., 0], quaternion1[..., 1], quaternion1[..., 2], quaternion1[..., 3]
+    q_mult = [
          x1*w0 + y1*z0 - z1*y0 + w1*x0,
         -x1*z0 + y1*w0 + z1*x0 + w1*y0,
          x1*y0 - y1*x0 + z1*w0 + w1*z0,
-        -x1*x0 - y1*y0 - z1*z0 + w1*w0), dtype=np.float64)
+        -x1*x0 - y1*y0 - z1*z0 + w1*w0]
+    if torch.is_tensor(quaternion1):
+        q_out = torch.stack(q_mult, dim=-1)
+    else:
+        q_out = np.stack(q_mult, axis=-1)
+    return q_out
 
 
 def quaternion_conjugate(quaternion):
