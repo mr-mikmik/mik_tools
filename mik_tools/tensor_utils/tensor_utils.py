e3964229a82e94f06311d9fef8bb40f4f78cc38a
mr-mikmik <mrmikmik95@gmail.com>
2024-08-13 14:02:26 -0400

    adding mask intersection functions to tensor_utils.py

--- mik_tools/tensor_utils/tensor_utils.py
+++ mik_tools/tensor_utils/tensor_utils.py
@@ -21,6 +21,88 @@ def batched_index_select(X:Union[np.ndarray, torch.Tensor], indxs:Union[np.ndarr
     return X_out
 
 
+def compute_bidirectional_mask_intersection_score(mask_1:torch.Tensor, mask_2:torch.Tensor, normalized=True) -> torch.Tensor:
+    """
+    Compute the intersection (sum) of two masks
+    Args:
+        mask_1 (torch.Tensor): shape (B, ...)
+        mask_2 (torch.Tensor): shape (K, ...)
+    Returns:
+        iou_scores (torch.Tensor): shape (B, K)
+    """
+    not_mask_1 = torch.logical_not(mask_1).to(torch.float32)  # (B, M)
+    not_mask_2 = torch.logical_not(mask_2).to(torch.float32)  # (B, M)
+    intersection_1s = compute_mask_intersection_score(mask_1, mask_2, normalized=normalized)  # (B, K)
+    intersection_0s = compute_mask_intersection_score(not_mask_1, not_mask_2, normalized=normalized)  # (B, K)
+    intersection = intersection_1s + intersection_0s
+    if normalized:
+        intersection = 0.5 * intersection # 1 means perfect ovelapping. -- each 1s and 0s count as 50% of the score.
+    return intersection
+
+
+def compute_mask_intersection_score(mask_1:torch.Tensor, mask_2:torch.Tensor, normalized=False) -> torch.Tensor:
+    """
+    Compute the intersection (sum) of two masks.
+        1 would be that there is one mask entirely contained inside the other.
+        0 the masks are not overlapping.
+    Args:
+        mask_1 (torch.Tensor): shape (B, ...)
+        mask_2 (torch.Tensor): shape (K, ...)
+    Returns:
+        iou_scores (torch.Tensor): shape (B, K)
+    """
+    # TODO: Consider normalize by the maxium overlapping possible.
+    B = mask_1.shape[0]
+    K = mask_2.shape[0]
+    mask_1_flat = mask_1.flatten(start_dim=1)  # (B, M)
+    mask_2_flat = mask_2.flatten(start_dim=1)  # (K, M)
+    # compute the 1s match (sum how much 1s match)
+    intersection = torch.einsum('bm,km->bk', mask_1_flat, mask_2_flat)  # (B, K)
+    if normalized:
+        mask_1_flat_sum = mask_1_flat.sum(dim=1)  # (B,)
+        mask_2_flat_sum = mask_2_flat.sum(dim=1)  # (K,)
+        norm_value = torch.min(torch.stack([
+            mask_1_flat_sum.unsqueeze(1).repeat_interleave(repeats=K, dim=1),
+            mask_2_flat_sum.unsqueeze(0).repeat_interleave(repeats=B, dim=0),
+        ], dim=-1), dim=-1)[0]  # (B, K)
+        out = torch.nan_to_num(intersection / norm_value)
+        # note: nan_to_num avoid the nans if there is the case of division by 0
+    else:
+        out = intersection
+    return out
+
+
+def compute_mask_iou_score(mask_1:torch.Tensor, mask_2:torch.Tensor, normalized=False, batch_size=None) -> torch.Tensor:
+    """
+    Compute the iou of two masks.
+        1 the masks perfectly intersect to each other, i.e. intersection = union
+        0 the masks are not overlapping. -- intersection = 0
+    Args:
+        mask_1 (torch.Tensor): shape (B, ...)
+        mask_2 (torch.Tensor): shape (K, ...)
+    Returns:
+        iou_scores (torch.Tensor): shape (B, K)
+    """
+    B = mask_1.shape[0]
+    K = mask_2.shape[0]
+    mask_1_flat = mask_1.flatten(start_dim=1)  # (B, M)
+    mask_2_flat = mask_2.flatten(start_dim=1)  # (K, M)
+    # compute the 1s match (sum how much 1s match)
+    intersection = torch.einsum('bm,km->bk', mask_1_flat, mask_2_flat)  # (B, K) where each element is the number of intersections
+    # union = torch.einsum('bm,km->bkm', mask_1_flat, mask_2_flat).sum(dim=-1)# (B, K) where each element is the number of
+    if batch_size is None:
+        union = ((mask_1_flat.unsqueeze(1)) | (mask_2_flat.unsqueeze(0))).sum(dim=-1)# (B, K) where each element is the number of
+    else:
+        union = []
+        for i in range(B):
+            mask_or_res = ((mask_1_flat[0].unsqueeze(0)) + mask_2_flat) > 0
+            union_i = mask_or_res.sum(dim=-1) # (K,)
+            union.append(union_i)
+        union = torch.stack(union, dim=0) # (B, K)
+    iou_scores = torch.nan_to_num(intersection / union)
+    return iou_scores
+
+
 # ======================================================================================================================
 # ================================== UTILS ================================================================
 # ======================================================================================================================
@@ -73,7 +155,9 @@ def batched_index_select_tensor(X: torch.Tensor, indxs:torch.Tensor) -> torch.Te
 
 
 # test
-if __name__ == '__main__':
+
+
+def test_batch_index_selection():
     X = np.arange(24).reshape(2, 3, 4, 1)
     indxs = np.random.randint(0, 4, size=(2, 3))
     Xt = torch.tensor(X)
@@ -82,3 +166,18 @@ if __name__ == '__main__':
     Xt_selected = batched_index_select_tensor(Xt, indxs_t)
     Xt_selected_2 = torch.gather(Xt, 2, indxs_t.unsqueeze(-1).unsqueeze(-1)).squeeze(-1)
     print('ok')
+
+
+def test_compute_mask_iou_score():
+    mask_1 = torch.tensor(np.random.randint(0,2, (2, 3, 3)))
+    mask_2 = torch.tensor(np.random.randint(0,2, (1, 3, 3)))
+    iou_score = compute_mask_iou_score(mask_1, mask_2)
+    print(mask_1)
+    print(mask_2)
+    print(iou_score)
+
+
+if __name__ == '__main__':
+    # test_batch_index_selection()
+    test_compute_mask_iou_score()
+
