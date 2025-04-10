488be8f57e4c95de152e8d2b08bdaa5352f2378c
mr-mikmik <mrmikmik95@gmail.com>
2025-04-10 09:58:15 -0400

    adding camera_matrix functions in camera_utils.py

--- mik_tools/camera_tools/camera_utils.py
+++ mik_tools/camera_tools/camera_utils.py
@@ -27,3 +27,36 @@ def compute_camera_pose(position:np.ndarray, lookat:np.ndarray, view_up=np.array
         return pose
     else:
         return matrix_to_pose(pose)
+
+
+def change_resolution_camera_matrix(K:np.ndarray, img_size_new, img_size_old=None) -> np.ndarray:
+    """
+    Preserve the field of view but increase resolution
+    :param K: (..., 3, 3)
+    :param img_size_new: (..., 2)
+    :param img_size_old: (..., 2) if None, use the current K
+    :return:
+    """
+    if img_size_old is None:
+        img_size_old = 2 * K[..., :2, -1] # (..., 2)
+    K_new = K.copy()
+    K_new[..., 0, 0] = K[..., 0, 0] * img_size_new[0] / img_size_old[0]
+    K_new[..., 1, 1] = K[..., 1, 1] * img_size_new[1] / img_size_old[1]
+    K_new[..., 0, 2] = img_size_new[0] * 0.5
+    K_new[..., 1, 2] = img_size_new[1] * 0.5
+    return K_new
+
+
+def modify_img_size_camera_matrix(K:np.ndarray, img_size_new) -> np.ndarray:
+    """
+    This updates the image size which is equivalent of cropping or padding the image
+    This operation preserves the spatial resolution of the camera.
+    If image_size_new is smaller than the original image size, it is like rendering a SUBSET of the image
+    :param K: (..., 3, 3)
+    :param img_size_new: (..., 2)
+    :param img_size_old: (..., 2) if None, use the current K
+    :return:
+    """
+    K_new = K.copy()
+    K_new[..., :2, -1] = np.floor(img_size_new * 0.5).astype(np.int64) # (..., 2)
+    return K_new
