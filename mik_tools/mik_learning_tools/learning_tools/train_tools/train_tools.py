d7fdf1c5b4fce86c066e656a8b3a1bebcf9167a7
mr-mikmik <aglaner@hotmail.com>
2025-04-18 16:38:33 -0400

    fixing train_tools in mik_learning_tools

--- mik_tools/mik_learning_tools/learning_tools/train_tools/train_tools.py
+++ mik_tools/mik_learning_tools/learning_tools/train_tools/train_tools.py
@@ -10,11 +10,11 @@ from mik_tools.mik_learning_tools.learning_tools.model_load_tools import get_che
     filter_model_args
 
 
-def train_model(args, Model, model_args, data_name, dataset, train_loader, val_loader=None, initialize_model_weights_version=None, resume_training_version=None, callbacks=None):
+def train_model(args, Model, model_args, data_path, dataset, train_loader, val_loader=None, initialize_model_weights_version=None, resume_training_version=None, callbacks=None):
     data_sizes = dataset.get_sizes()
 
     if resume_training_version is not None:
-        model = load_model(Model, load_version=resume_training_version, data_path=data_name,
+        model = load_model(Model, load_version=resume_training_version, data_path=data_path,
                            input_sizes=dataset.get_sizes())
     else:
         model = Model(data_sizes, **model_args)
@@ -23,7 +23,7 @@ def train_model(args, Model, model_args, data_name, dataset, train_loader, val_l
         model.load_weights_from_version(initialize_model_weights_version)
 
     print(f'\n\n\t Training Model {model.name} Started\n\n')
-    logger = TensorBoardLogger(os.path.join(data_name, 'tb_logs'), name=model.name)
+    logger = TensorBoardLogger(os.path.join(data_path, 'tb_logs'), name=model.name)
 
     # trainer = Trainer.from_argparse_args(args)  # (gpus=gpus, max_epochs=MAX_EPOCHS, logger=logger, log_every_n_steps=1)
     trainer_args = filter_trainer_args(args)
@@ -32,7 +32,7 @@ def train_model(args, Model, model_args, data_name, dataset, train_loader, val_l
     trainer = Trainer(**trainer_args)  # (gpus=gpus, max_epochs=MAX_EPOCHS, logger=logger, log_every_n_steps=1)
     if resume_training_version is not None:
         checkpoint_path = get_checkpoint_path(Model.get_name(), load_version=resume_training_version,
-                                              data_path=data_name)
+                                              data_path=data_path)
         trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=val_loader, ckpt_path=checkpoint_path)
     else:
         trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=val_loader)
